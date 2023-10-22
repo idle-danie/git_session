60b524f4fd4dbeecce31014b7340978b951a09da
unknown <daniel9654@ajou.ac.kr>
2023-10-23 01:35:37 +0900

    test

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 provider "aws" {
-  region = "ap-northeast-2"
-  profile = "myprofile"
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
+  region     = var.AWS_REGION
 }
 
 resource "aws_iam_role" "lambda_role" {
