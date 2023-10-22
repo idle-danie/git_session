f690712b70108f2ccf106fe206c6d54457a59a2c
unknown <daniel9654@ajou.ac.kr>
2023-10-23 03:18:37 +0900

    test

--- main.tf
+++ main.tf
@@ -2,7 +2,17 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
+data "aws_iam_role" "existing_role" {
+  name = "lambda_role"
+}
+
+locals {
+  lambda_role_arn = try(data.aws_iam_role.existing_role.arn, null)
+}
+
 resource "aws_iam_role" "lambda_role" {
+  count = local.lambda_role_arn == null ? 1 : 0
+
   name = "lambda_role"
 
   assume_role_policy = jsonencode({
@@ -18,15 +28,15 @@ resource "aws_iam_role" "lambda_role" {
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
+  role       = local.lambda_role_arn != null ? data.aws_iam_role.existing_role.name : aws_iam_role.lambda_role[0].name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-  role       = aws_iam_role.lambda_role.name
 }
 
 resource "aws_lambda_function" "this" {
   filename      = "lambda_function_payload.zip"
   function_name = "lambda_function"
   handler       = "lambda_function.lambda_handler"
-  role          = aws_iam_role.lambda_role.arn
+  role          = local.lambda_role_arn != null ? data.aws_iam_role.existing_role.arn : aws_iam_role.lambda_role[0].arn
   runtime       = "python3.9"
 }
 
