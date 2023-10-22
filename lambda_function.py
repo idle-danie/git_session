61b3c19fd4c6b86c4c4851e7cceac61429e94ec7
unknown <daniel9654@ajou.ac.kr>
2023-10-22 20:20:45 +0900

    test

--- lambda_function.py
+++ lambda_function.py
@@ -4,5 +4,6 @@ import json
 def lambda_handler(event, context):
     original_text = event.get("text", "")
     uppercased_text = original_text.upper()
+    print(f"Original text: {original_text}")
 
     return {"statusCode": 200, "body": json.dumps({"original": original_text, "uppercased": uppercased_text})}
